b4e248f5eafb4453ba154b5d700552e3b7ecb305
Marvin Danig <marvin@marvindanig.com>
2016-06-08 14:15:24 -0400

    Async file read and error handling

--- arc.js
+++ arc.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 
 module.exports = {
-  read(location) {
-    return JSON.parse(fs.readFileSync(location).toString());
-  },
+    read(location) {
+        fs.readFile(location, function read(err, data) {
+            if (err) {
+                console.log(chalk.bold.red("Unregistered client"));
+                console.log(chalk.bold.cyan("Try $ bookiza register --help"));
+            }
+            return JSON.parse(data.toString());
+        });
 
-  write(location, arc) {
-    fs.writeFileSync(location, JSON.stringify(arc, null, 2));
-    return;
-  }
+        // return JSON.parse(fs.readFileSync(location).toString());
+    },
+
+    write(location, arc) {
+        fs.writeFileSync(location, JSON.stringify(arc, null, 2));
+        return;
+    }
 };
