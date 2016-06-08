e2d5f35e9e20ebb1730f817c16deabc18adf015b
Marvin Danig <marvin@marvindanig.com>
2016-06-08 12:08:41 -0400

    Reversing ES2015 module.exports

--- arc.js
+++ arc.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-module.export = {
+module.exports = {
   read(location) {
     return JSON.parse(fs.readFileSync(location).toString());
   },
